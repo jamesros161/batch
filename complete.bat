77e33b053b545007ba3fe7220f099d9ce9ae43e7
James Rosado <jamesros@boldgrid.com>
2020-05-14 15:06:42 -0400

    initial

---
+++ complete.bat
@@ -0,0 +1,17 @@
+@echo off
+set NLM=^
+
+
+set NL=^^^%NLM%%NLM%^%NLM%%NLM%
+
+set name=%1
+set content-path=%2
+set root-path=%3
+set category=%4
+
+echo %name%
+echo %content-path%
+echo %root-path%
+echo %category%
+
+pause
